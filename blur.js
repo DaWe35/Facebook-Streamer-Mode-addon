613a80cbddbf0503ff30bfad1fa935cd881dd20b
DaWe <10603822+DaWe35@users.noreply.github.com>
2019-05-30 18:44:38 +0200

    upload

---
+++ blur.js
@@ -0,0 +1,12 @@
+var all = document.querySelectorAll('._55lr, .fwb, ._6qw4, ._3mf5, .ltr, .UFICommentActorName, .profileLink, ._38vo');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.filter = "blur(5px)"
+}
+
+/*var all = document.getElementsByClassName('fwb');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.filter = "blur(5px)"
+}
+
+
+_6qw4*/
